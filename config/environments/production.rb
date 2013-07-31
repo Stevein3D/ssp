ce0ca928131e6ac985f442e7b423d5eb408199cd
Steve Sikoryak <stevein3d@gmail.com>
2013-07-31 17:36:54 -0400

    Add SMTP settings to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,17 @@ Ssp::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  authentication: "plain",
+  user_name: "shuttershockfilms@gmail.com",
+  password: ENV['SMTP_PASSWORD'],
+  enable_starttls_auto: true
+}
+config.action_mailer.raise_delivery_errors = true
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
