590055ca54dc6f55e380465865db3da24dda0d32
Steve Sikoryak <stevein3d@gmail.com>
2013-08-23 11:18:40 -0400

    Combine work pages into single page.  Adjust fade animations

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,26 +1,29 @@
 class StaticPagesController < ApplicationController
   def index
-    @videos = Vimeo::Simple::Album.videos("2389650")
+    @recent = Vimeo::Simple::Album.videos("2389650")
   end
 
   def work
     @user = Vimeo::Simple::User.info("kevw85")
-    @videos = Vimeo::Simple::Album.videos("2389650")
+    @recent = Vimeo::Simple::Album.videos("2389650")
+    @arts = Vimeo::Simple::Album.videos("1873139")
+    @corporate = Vimeo::Simple::Album.videos("1903771")
+    @live = Vimeo::Simple::Album.videos("1875093")
   end
 
   def work_arts
     @user = Vimeo::Simple::User.info("kevw85")
-    @videos = Vimeo::Simple::Album.videos("1873139")
+    @arts = Vimeo::Simple::Album.videos("1873139")
   end
 
   def work_corporate
     @user = Vimeo::Simple::User.info("kevw85")
-    @videos = Vimeo::Simple::Album.videos("1903771")
+    @corporate = Vimeo::Simple::Album.videos("1903771")
   end
 
   def work_live
     @user = Vimeo::Simple::User.info("kevw85")
-    @videos = Vimeo::Simple::Album.videos("1875093")
+    @live = Vimeo::Simple::Album.videos("1875093")
   end
 
   def facilities
