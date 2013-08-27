9e4db6e5cf030b840c6faa77770caea07fc2ba5d
Steve Sikoryak <stevein3d@gmail.com>
2013-08-27 18:24:07 -0400

    Remove unused sub-work pages and adjust youtube links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,21 +11,6 @@ class StaticPagesController < ApplicationController
     @live = Vimeo::Simple::Album.videos("1875093")
   end
 
-  def work_arts
-    @user = Vimeo::Simple::User.info("kevw85")
-    @arts = Vimeo::Simple::Album.videos("1873139")
-  end
-
-  def work_corporate
-    @user = Vimeo::Simple::User.info("kevw85")
-    @corporate = Vimeo::Simple::Album.videos("1903771")
-  end
-
-  def work_live
-    @user = Vimeo::Simple::User.info("kevw85")
-    @live = Vimeo::Simple::Album.videos("1875093")
-  end
-
   def facilities
   end
 
